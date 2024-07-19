4cb9c0a9a23250fcabaf2caf31229b4c3280b4a0
Don Jayamanne <Don.Jayamanne@microsoft.com>
2024-07-19 15:27:26 +1000

    Add tests

--- src/test/pythonEnvironments/nativePythonFinder.unit.test.ts
+++ src/test/pythonEnvironments/nativePythonFinder.unit.test.ts
@@ -4,7 +4,7 @@
 import { assert } from 'chai';
 import * as sinon from 'sinon';
 import * as typemoq from 'typemoq';
-import { WorkspaceConfiguration } from 'vscode';
+import { Uri, WorkspaceConfiguration } from 'vscode';
 import {
     getNativePythonFinder,
     isNativeEnvInfo,
@@ -14,6 +14,8 @@ import {
 import * as windowsApis from '../../client/common/vscodeApis/windowApis';
 import { MockOutputChannel } from '../mockClasses';
 import * as workspaceApis from '../../client/common/vscodeApis/workspaceApis';
+import { flattenIterable } from '../../client/common/utils/async';
+import { NativePythonEnvironmentKind } from '../../client/pythonEnvironments/base/locators/common/nativePythonUtils';
 
 suite('Native Python Finder', () => {
     let finder: NativePythonFinder;
@@ -86,5 +88,53 @@ suite('Native Python Finder', () => {
         } else {
             assert.fail('Expected either executable or prefix to be defined');
         }
+    });
+    test('Find Python environment when the parent (bin/scripts) directory is given', async () => {
+        const envs = (await flattenIterable(finder.refresh())).filter(isNativeEnvInfo);
+
+        const condaEnv = envs.find((e) => e.kind === NativePythonEnvironmentKind.Conda && e.executable && e.prefix);
+        const prefix = condaEnv?.prefix;
+        if (!condaEnv || !prefix) {
+            return;
+        }
+
+        // Running a find on this env should return the same details.
+        const foundEnvs = (await flattenIterable(finder.refresh([Uri.file(prefix)]))).filter(isNativeEnvInfo);
+        const foundEnv = foundEnvs.find((e) => e.executable === condaEnv.executable);
+
+        assert.ok(foundEnv, `Conda env ${JSON.stringify(condaEnv)} not found, only found ${foundEnvs}`);
+        assert.deepEqual(condaEnv, foundEnv);
+    });
+    test('Find Python environment with exe', async () => {
+        const envs = (await flattenIterable(finder.refresh())).filter(isNativeEnvInfo);
+
+        const condaEnv = envs.find((e) => e.kind === NativePythonEnvironmentKind.Conda && e.executable && e.prefix);
+        const executable = condaEnv?.executable;
+        if (!condaEnv || !executable) {
+            return;
+        }
+
+        // Running a find on this env should return the same details.
+        const foundEnvs = (await flattenIterable(finder.refresh([Uri.file(executable)]))).filter(isNativeEnvInfo);
+        const foundEnv = foundEnvs.find((e) => e.executable === condaEnv.executable);
+
+        assert.ok(foundEnv, `Conda env ${JSON.stringify(condaEnv)} not found, only found ${foundEnvs}`);
+        assert.deepEqual(condaEnv, foundEnv);
+    });
+    test('Find Python environment in current workspace folder return a list of envs', async () => {
+        const envs = (await flattenIterable(finder.refresh())).filter(isNativeEnvInfo);
+
+        const condaEnv = envs.find((e) => e.kind === NativePythonEnvironmentKind.Conda && e.executable && e.prefix);
+        const executable = condaEnv?.executable;
+        if (!condaEnv || !executable) {
+            return;
+        }
+
+        // Running a find on this env should return the same details.
+        const foundEnvs = (await flattenIterable(finder.refresh([Uri.file(executable)]))).filter(isNativeEnvInfo);
+        const foundEnv = foundEnvs.find((e) => e.executable === condaEnv.executable);
+
+        assert.ok(foundEnv, `Conda env ${JSON.stringify(condaEnv)} not found, only found ${foundEnvs}`);
+        assert.deepEqual(condaEnv, foundEnv);
     });
 });
